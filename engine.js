629a9a268ebcc8dd99b36388e145b895f6d683fd
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 08:32:11 -0800

    testing mongo host variables

--- engine.js
+++ engine.js
@@ -7,7 +7,9 @@ function enginecallback() {
 
 EngineProvider = function() {
   //this.db = new MongoDB('127.0.0.1',27017); //localhost
-  this.db = new MongoDB(process.env.OPENSHIFT_MONGODB_DB_HOST,process.env.OPENSHIFT_MONGODB_DB_PORT); //openshift
+  this.db = new MongoDB('127.4.131.129',27017); //localhost
+  
+  //this.db = new MongoDB(process.env.OPENSHIFT_MONGODB_DB_HOST,process.env.OPENSHIFT_MONGODB_DB_PORT); //openshift
 
   console.log("server::engine database is " + this.db );
 };
