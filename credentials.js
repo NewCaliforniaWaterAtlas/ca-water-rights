c52dea72d5ef4637e9cb0da305cc93b3b0dd1c35
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 12:30:27 -0800

    Adding credentials back in temporarily.

---
+++ credentials.js
@@ -0,0 +1,10 @@
+var host = process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1";
+var port = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+
+var credentials = {
+  mongo_password: 'vP6JNZJbmdG-',
+  db_host: host,
+  db_port: port
+};
+
+module.exports = credentials;
