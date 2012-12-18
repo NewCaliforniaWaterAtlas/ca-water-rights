18b9d02600e368b3862d4de22159707c6a3b4c7e
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 14:16:29 -0800

    Updated geojson data, removed old js to water rights

--- public/javascripts/custom/water.map.js
+++ public/javascripts/custom/water.map.js
@@ -19,7 +19,9 @@ water.setMapCenterZoom = function(lat,lon,zoom, map) {
 };
 
 water.setupMap = function() {
-  // var layer = new MM.StamenTileLayer("watercolor");
+  var layer = new MM.StamenTileLayer("watercolor");
+  
+  // @TODO need new maps.
   
   // If we cannot load the map for some reason then just use a default image
 
@@ -70,90 +72,36 @@ water.setupMap = function() {
   markers.parent.setAttribute("id", "markers");
 
   //http://www.mongodb.org/display/DOCS/Geospatial+Indexing
-  var lat = 37.8053;
-  var lon = -122.2725;
+  var lat = 39.65850816;
+  var lon = -121.3551829;
   //Core.query({"coordinates" : { "$near" : [lon,lat]}}, water.paintTreeMarkers);
 
 
 //  Core.query({ "properties": {"$or" : [ { "zipcode": "94607" } , { "zipcode": "94606" }, { "zipcode": "94609" }, { "zipcode": "94612" }, { "zipcode": "94610" } ] }}, water.paintTreeMarkers);  
 
-  Core.query({}, water.paintTreeMarkers);  
+  // Core.query({}, water.paintTreeMarkers);  
 
-  map.setCenterZoom(new MM.Location(lat,lon), 15);
+  map.setCenterZoom(new MM.Location(lat,lon), 9);
 
-  //Core.query2("/public/data/farmers_markets_nocal.json",water.paintMarketMarkers);
-  //Core.query2("/public/data/berk_oak_sfo_population-csv.json",water.paintZipcodeMarkers);
+  Core.query2("/data/water_rights_merged_butte_geojson.json",water.paintRightsMarkers);
+
 };
 
 
-water.makeTreeMarker = function(feature) {
+water.makeRightsMarker = function(feature) {
 
   var id = feature.properties.id;
   var marker = document.createElement("div");
  
   var watertring = '';
-  watertring += "<h2>" + feature.properties.title + "</h2>";
+  watertring += "<h2>" + feature.properties.Water Right Type + "</h2>";
 /*
   if(feature.properties.address_street !== undefined) {
     watertring += "<h3><strong>" + feature.properties.address_street + "</strong></h3>";
   }
   
 */
-  watertring += "<hr>";
 
-
-  if(feature.properties.species_root !== undefined) {
-    watertring += "<p><em>" + feature.properties.species_root + "</em></p>";
-  }  
-
-  if(feature.properties.edible_fruit_tree !== null && feature.properties.edible_fruit_tree !== undefined) {
-    watertring += "<p>Edible? <strong>" + feature.properties.edible_fruit_tree + "</strong></p>";
-  }  
-  if(feature.properties.graftable !== null && feature.properties.graftable !== "undefined") {
-    watertring += "<p>Graftable? <strong>" + feature.properties.graftable + "</strong></p>";
-  }
-  if(feature.properties.address !== undefined) {
-    watertring += "<p>Address <strong>" + feature.properties.address + " Oakland, CA</strong></p>";
-  }
-  
-  if(feature.properties.Seasonality !== undefined && feature.properties.Seasonality !== null) {
-    var seasons = water.readSeason(feature);
-    watertring += "<p>In season <strong>" + seasons.list.toString() + "</strong></p>";
-  }
-
- /*
- if(feature.stewardship !== undefined) {
-    watertring += "<p>Stewardship <strong>" + feature.stewardship + "</strong></p>";
-  }  
-
-  if(feature.variety !== undefined) {
-    watertring += "<p>Variety <strong>" + feature.variety + "</strong></p>";
-  }   
-
-  if(feature.quantity !== undefined) {
-    watertring += "<p>Quantity <strong>" + feature.quantity + "</strong></p>";
-  } 
-
-  if(feature.description !== undefined) {
-    watertring += "<p>Description <strong>" + feature.description + "</strong></p>";
-  } 
-
-  if(feature.properties.steward_name !== undefined || feature.contact_name !== undefined) {
-    watertring += "<p>Steward Name <strong>" + feature.contact_name + "</strong></p>";
-  }
-*/
-
-  if(feature.properties.tree_owner !== undefined) {
-    watertring += "<p>Tree Owner <strong>" + feature.properties.tree_owner + "</strong></p>";
-  }
-  
-  if(feature.properties.datasource !== undefined) {
-    watertring += "<p>Data Source <strong>" + feature.properties.datasource + "</strong></p>";
-  }
-
-  if(feature.properties.last_updated !== undefined) {
-    watertring += "<p>Last Updated <strong>" + feature.properties.last_updated + "</strong></p>";
-  }
   
   marker.feature = feature;
   markers.addMarker(marker, feature);
@@ -175,7 +123,7 @@ water.makeTreeMarker = function(feature) {
   if(feature.art) {
     img.setAttribute("src", feature.art );
   } else {
-    img.setAttribute("src", "/images/icons/lemon_tree_icon.png");
+    img.setAttribute("src", "/images/icons/water_right_icon.png");
   }
 
   
@@ -213,167 +161,10 @@ water.makeTreeMarker = function(feature) {
   MM.addEvent(marker, "mouseover", water.onMarkerOver);
   MM.addEvent(marker, "mouseout", water.onMarkerOut);
   MM.addEvent(marker, "click", water.onMarkerClick);
-}
-
-water.readSeason = function(feature) {
-  var feature = feature;
-  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var seasonality = feature.properties.Seasonality.split(",");
-  var inSeason = {};
-  inSeason.list = [];
-  for(var i = 0; i < months.length; i++ ) {
-    if(seasonality[i] == 2) {
-      inSeason.list += months[i] + " ";
-
-    }
-  }
-  return inSeason;
-}
-
-water.makeMarketMarker = function(feature) {
-
-  var id = feature.properties.id;
-  var marker = document.createElement("div");
- 
-  var markupString = '';
-  markupString += "<h2>" + feature.properties.name + "</h2>";
-
-  if(feature.properties.url !== undefined && feature.properties.url !== null ) {
-    markupString += '<p><a href="' +  feature.properties.url + '">Website</a></p>';
-  }
-  
-  marker.feature = feature;
-  markers.addMarker(marker, feature);
-
-  // Unique hash marker id for link
-  marker.setAttribute("id", "marker-" + id);
-  marker.setAttribute("type", "market");
-  marker.setAttribute("dataName", feature.properties.title);
-  marker.setAttribute("class", "marker");
-
-  // Specially set value for loading data.
-  marker.setAttribute("marker_id", id);
-
-  // create an image icon
-  var img = marker.appendChild(document.createElement("img"));
-
-  if(feature.art) {
-    img.setAttribute("src", feature.art );
-  } else {
-    img.setAttribute("src", "/images/icons/farmers_market.png");
-  }
-
-  $('a.add-tree').click(function(){
-    var mapCenter = map.center();
-    $('input#lat').val(mapCenter.lat);
-    $('input#lon').val(mapCenter.lon);
-  });
-  
-  // Tooltips
-  $("#marker-" + id + " img").qtip({
-	content: {
-    text: markupString,
-	},
-	hide: {
-		delay: 1500
-	},
-	position: {
-		my: 'middle left', 
-		at: 'bottom middle',
-		adjust: {
-			x: 20,
-			y: -10
-		}
-	},
-	style: { 
-		tip: true,
-		classes: 'ui-tooltip-dark'
-	},
-	tip: {}
-  });
-
-  $('a[title]').qtip();
-
-  // Listen for mouseover & mouseout events.
-  MM.addEvent(marker, "mouseover", water.onMarkerOver);
-  MM.addEvent(marker, "mouseout", water.onMarkerOut);
-  MM.addEvent(marker, "click", water.onMarkerClick);
-}
+};
 
 
-water.makeZipcodeMarker = function(feature) {
-
-  var id = feature.properties.id;
-  var marker = document.createElement("div");
- 
-  var markupString = '';
-  markupString += "<h2>Zipcode: " + feature.properties.name + "</h2>";
-
-  markupString += "<p>Population in zipcode: " + feature.properties.zipcode_population + "</p>";
-  markupString += "<p>Land area, square miles: " + feature.properties.land_area_square_miles + "</p>";
-  markupString += "<p>Water area, square miles: " + feature.properties.water_area_square_miles + "</p>";
-  markupString += "<p>Population Density, people per square mile: " + feature.properties.population_density_sq_mi + "</p>";
-
-  markupString += "<p>Media House Value, 2010: " + feature.properties.median_house_price + "</p>";
-  markupString += "<p>" + feature.properties.city + "</p>";
-
-  markupString += "<p>Datasource http://city-data.com</p>";
-
-
-  marker.feature = feature;
-  markers.addMarker(marker, feature);
-
-  // Unique hash marker id for link
-  marker.setAttribute("id", "marker-" + id);
-  marker.setAttribute("type", "market");
-  marker.setAttribute("dataName", feature.properties.title);
-  marker.setAttribute("class", "marker");
-
-  // Specially set value for loading data.
-  marker.setAttribute("marker_id", id);
-
-  // create an image icon
-  var img = marker.appendChild(document.createElement("img"));
-
-  if(feature.art) {
-    img.setAttribute("src", feature.art );
-  } else {
-    img.setAttribute("src", "/images/icons/information-icon.png");
-  }
-
-  // Tooltips
-  $("#marker-" + id + " img").qtip({
-	content: {
-    text: markupString,
-	},
-	hide: {
-		delay: 1500
-	},
-	position: {
-		my: 'middle left', 
-		at: 'bottom middle',
-		adjust: {
-			x: 20,
-			y: -10
-		}
-	},
-	style: { 
-		tip: true,
-		classes: 'ui-tooltip-dark'
-	},
-	tip: {}
-  });
-
-  $('a[title]').qtip();
-
-  // Listen for mouseover & mouseout events.
-  MM.addEvent(marker, "mouseover", water.onMarkerOver);
-  MM.addEvent(marker, "mouseout", water.onMarkerOut);
-  MM.addEvent(marker, "click", water.onMarkerClick);
-}
-
-
-water.paintTreeMarkers = function(features) {
+water.paintRightsMarkers = function(features) {
 
 /* {"name": "water","type":"FeatureCollection","features":[ */
 
@@ -391,25 +182,6 @@ var zoomLevel = map.getZoom();
   
 };
 
-water.paintMarketMarkers = function(features) {
-  features = features.features;
-  var len = features.length; 
-  console.log("water::paintMarketMarkers showing markers " + len );
-  for (var i = 0; i < len; i++) {
-    var feature = features[i];
-    water.makeMarketMarker(feature);
-  }
-};
-
-water.paintZipcodeMarkers = function(features) {
-  features = features.features;
-  var len = features.length; 
-  console.log("water::paintZipcodeMarkers showing markers " + len );
-  for (var i = 0; i < len; i++) {
-    var feature = features[i];
-    water.makeZipcodeMarker(feature);
-  }
-};
 
 water.repaint_agent = function(agent) {
 
