91bf17c4a3a35f533fa27a3f1332b3043c683598
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 12:46:04 -0800

    Removed old files. Added geojson water rights data

--- public/javascripts/custom/water.views.js
+++ public/javascripts/custom/water.views.js
@@ -1,21 +1,21 @@
 /**
  * Lemonopoly data variable. Contains filtered object lists like 'activities', 'challenges', and 'challenge_statuses'
  */
-trees.jsonData = {};
+water.jsonData = {};
 
 /**
  * Data loading wrapper for JSON formatted data, runs callback/closure to make sure data is really loaded.
  * vars contains the callback and other data to pass through to the later functions.
  */
-trees.loadJSON = function(vars) {
+water.loadJSON = function(vars) {
   var vars = vars;
   var contentData = vars.path;
   var getData = $.ajax({
     url:  contentData,
     dataType: 'json',
     data: vars.data,
-    success: trees.setData,
-    error: trees.loadDataError
+    success: water.setData,
+    error: water.loadDataError
   });
   getData.vars = vars;
 };
@@ -23,13 +23,13 @@ trees.loadJSON = function(vars) {
 /**
  * Set data into jsonData object, then run callback.
  */
-trees.setData = function(data, statusText, jqxhr) {
+water.setData = function(data, statusText, jqxhr) {
   if (data.length === undefined) {
     console.log("no results or undefined");
     return false;
   } 
-  trees.jsonData[jqxhr.vars.dataName] = data;
-  trees.jsonData[jqxhr.vars.dataName]["vars"] = jqxhr.vars;
+  water.jsonData[jqxhr.vars.dataName] = data;
+  water.jsonData[jqxhr.vars.dataName]["vars"] = jqxhr.vars;
 
   var callback = jqxhr.vars.callback;
   callback();
