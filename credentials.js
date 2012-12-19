9592301497a6767b725e62fa471f64504709ac04
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 23:12:46 -0800

    Casting openshift port value as integer

--- credentials.js
+++ credentials.js
@@ -1,5 +1,5 @@
 var host = process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1";
-var port = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
+var port = Math.floor(process.env.OPENSHIFT_MONGODB_DB_PORT) || 27017;
 
 var credentials = {
   mongo_user: 'www',
