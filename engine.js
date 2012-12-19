697f85dd221cd0eea350adb7e1764fa89dc9043e
Chacha Sikes <chachasikes@gmail.com>
2012-12-19 09:56:07 -0800

    Cleaned up code

--- engine.js
+++ engine.js
@@ -7,19 +7,8 @@ function enginecallback() {
 
 
 EngineProvider = function() {
-
   var credentials = require('./credentials.js'); 
-  console.log(credentials);
-
-
-  //this.db = new MongoDB('127.0.0.1',27017); //localhost
-  //this.db = new MongoDB('127.4.131.129',27017); //localhost
-  
-  // @TODO this is not really working. The process.env variables cannot be read… not sure why. - Chach 12/18/2012
-
- 
   this.db = new MongoDB(credentials); //openshift
-
   console.log("server::engine database is " + this.db );
 };
 
