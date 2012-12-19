9c1612a28ac35371eb0d569687228012b6bc2eac
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 21:57:31 -0800

    Abstracted all credentials into one file.

--- credentials.js
+++ credentials.js
@@ -2,9 +2,12 @@ var host = process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1";
 var port = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
 
 var credentials = {
-  mongo_password: 'vP6JNZJbmdG-',
+  mongo_user: 'www',
+  mongo_password: 'changeme',
   db_host: host,
   db_port: port
 };
 
 module.exports = credentials;
+
+//console.log(module.exports);
