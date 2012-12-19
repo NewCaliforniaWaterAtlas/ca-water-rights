0bcb7f7e132c6bca6fad8cb01e14e4552778290e
Chacha Sikes <chachasikes@gmail.com>
2012-12-18 21:33:53 -0800

    testing mongo auth

--- credentials.js
+++ credentials.js
@@ -2,7 +2,7 @@ var host = process.env.OPENSHIFT_MONGODB_DB_HOST || "127.0.0.1";
 var port = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
 
 var credentials = {
-  mongo_password: 'vP6JNZJbmdG-',
+  mongo_password: '',
   db_host: host,
   db_port: port
 };
