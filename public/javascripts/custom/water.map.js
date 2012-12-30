a11125cefe08fb6feb612bfcedaef21c3238f001
Chacha Sikes <chachasikes@gmail.com>
2012-12-30 10:14:35 -0800

    Adjusting marker layer interactions

--- public/javascripts/custom/water.map.js
+++ public/javascripts/custom/water.map.js
@@ -5,8 +5,8 @@ water.map = water.map || {};
 water.map_defaults = {};
 water.map_defaults.lat = 38.52;
 water.map_defaults.lon = -121.50;
-water.map_defaults.boxsize_lat = 0.7; //pretty small box
-water.map_defaults.boxsize_lon = 0.14;
+water.map_defaults.boxsize_lat = 0.1; //pretty small box
+water.map_defaults.boxsize_lon = 0.6;
 water.map_defaults.zoom = 6;
 water.map_defaults.satellite_layer = 'chachasikes.map-oguxg9bo';
 water.map_defaults.zoomed_out_marker_layer = 'chachasikes.WaterTransfer-Markers';
@@ -15,6 +15,10 @@ water.map_defaults.close_up_zoom_level = 8;
 
 // Establish empty container for loaded marker features data.
 water.markerLayer = 0;
+water.markers_station_usgs = 0;
+water.markers_station_cdec = 0;
+water.markers_rights = 0;
+
 
 // Set up map interaction variables.
 water.map_interaction = {};
@@ -34,11 +38,29 @@ water.setupMap = function() {
   mapbox.load(water.map_defaults.zoomed_out_marker_layer, function(interactive){
       water.map.addLayer(interactive.layer);
       water.map.interaction.auto(); 
+
+
+/*    //@TODO seeing if we have any control at all over the interactions
+
+      water.mapboxMarkerInteraction = mapbox.markers.interaction(water.interactiveLayer);
+
+      water.mapboxMarkerInteraction.formatter(function(feature) {
+        var string = water.formatTooltipStrings;
+        return string;
+      });
+
+      water.map.interaction.refresh(); 
+*/
+
+
+
+
+
   });
 
   // Add map interface elements.
   water.map.ui.zoomer.add();
-  water.map.setZoomRange(5, 17);
+  water.map.setZoomRange(6, 17);
 /*// @TODO see if we can change the increment of the zoomer.
   // This doesn't seem to work though. Maybe make new zoomer? Maybe override easey?
   // Needs more research.
@@ -79,7 +101,6 @@ water.centerMap = function() {
 water.loadMarkers = function() {
   
   var zoom = water.map.zoom();
-  console.log(zoom);
   
   if(zoom > water.map_defaults.close_up_zoom_level) {
     water.markersQuery(false);
@@ -95,8 +116,29 @@ water.loadMarkers = function() {
     }
     else {
       water.map.removeCallback('panned', water.markersPanned);
+/*       water.map.removeLayer(water.markerLayer); */
     }
   });
+
+  water.map.addCallback('drawn', function() {
+    // .markers() gives a list of markers, along with their elements attached.
+    var markers = water.markerLayer.markers(),
+        // construct an empty list to fill with onscreen markers
+        inextent = [],
+        // get the map extent - the top-left and bottom-right locations
+        extent = water.map.extent()
+
+    // for each marker, consider whether it is currently visible by comparing
+    // with the current map extent
+    for (var i = 0; i < markers.length; i++) {
+      if (extent.containsLocation(markers[i].location)) {
+          inextent.push(markers[i].data.properties.name);
+      }
+    }
+
+    // display a list of markers.
+    onscreen.innerHTML = inextent.join('\n');
+});
 
 };
 
@@ -144,9 +186,9 @@ water.markersQuery = function(reloaded) {
  Core.query({ 
      $and: [{'kind': 'right'}, {$where: "this.properties.latitude < " + (lat + boxsize_lat)},{$where: "this.properties.latitude > " + (lat - boxsize_lat)},{$where: "this.properties.longitude < " + (lon + boxsize_lon)},{$where: "this.properties.longitude > " + (lon - boxsize_lon)}
   ] 
-    }, water.drawRightsMarkers, {'limit': 300}); 
+    }, water.drawRightsMarkers, {'limit': 0}); 
   
-  // Load CDEC stations.
+  // Load CDEC staons.
   Core.query({ 
      $and: [{'kind': 'station'}, {$where: "this.properties.latitude < " + (lat + boxsize_lat)},{$where: "this.properties.latitude > " + (lat - boxsize_lat)},{$where: "this.properties.longitude < " + (lon + boxsize_lon)},{$where: "this.properties.longitude > " + (lon - boxsize_lon)}
   ] 
@@ -164,12 +206,20 @@ water.drawMarkers = function(features, featureDetails) {
   var features = features;
   
   // Allow layer to be reset and also to add a series of sets of features into the layer (for interaction purposes.)
-  if(water.markerLayer === 0) {
-    water.markerLayer = mapbox.markers.layer().id('markers');
+  if(water[featureDetails.layer] === 0) {
+    water[featureDetails.layer] = mapbox.markers.layer().id(featureDetails.layer);
+    water[featureDetails.layer + "_interaction"] = mapbox.markers.interaction(water[featureDetails.layer]);
 /*     mapbox.markers.interaction(water.markerLayer); */
-    water.map.addLayer(water.markerLayer);
+    water.map.addLayer(water[featureDetails.layer]);
 /*     water.map.interaction.refresh(); */
   }
+
+
+console.log(featureDetails.layer + "_interaction");
+    water[featureDetails.layer + "_interaction"].formatter(function(feature) {
+    var string = water.formatTooltipStrings;
+    return string;
+  });
 
   // Generate marker layers.
   
@@ -185,11 +235,55 @@ water.drawMarkers = function(features, featureDetails) {
 */
 
 
-  water.markerLayer.features(features).factory(function(f) { 
+  water[featureDetails.layer].features(features).factory(function(f) { 
     var marker = water.makeMarker(f, featureDetails);
     return marker;
   });
- 
+
+};
+
+
+water.formatTooltipStrings = function() {
+    var string = '';
+    if (feature.properties.holder_name !== undefined) {
+      string +=  
+        "<p>" + "Owner: " + feature.properties.holder_name + "</p>"
+      + "<p>" + "Type: " + feature.properties.organization_type + "</p>"
+      + "<p>" + "Source: " + feature.properties.source_name + "</p>"
+      + "<p>" + "Watershed: " + feature.properties.watershed + "</p>"
+      + "<p>" + "County: " + feature.properties.county + "</p>"
+      + "<p>" + "Right Type: " + feature.properties.water_right_type + "</p>"
+      + "<p>" + "Right Status: " + feature.properties.status + "</p>"
+      + "<p>" + "Diversion: " + feature.properties.diversion + feature.properties.diversion_units + "</p>"
+      + "<p>" + "Storage: " + feature.properties.diversion_storage_amount + "</p>";
+    }
+  
+    if (feature.properties.station_code !== undefined) {
+      string +=  
+        "<p>" + "Station Code: " + feature.properties.station_code + "</p>"
+      + "<p>" + "Station Name: " + feature.properties.station_name + "</p>"
+      + "<p>" + "Station Data Type: " + feature.properties.station_type + "</p>"
+      + "<p>" + "Altitude: " + feature.properties.altitude + "</p>"
+      + "<p>" + "County: " + feature.properties.county + "</p>"
+      + "<p>" + "River Basin: " + feature.properties.river_basin + "</p>"
+      + "<p>" + "Sensors: " + feature.properties.sensors + "</p>"
+      + "<p>" + "Flow Data: " + feature.properties.flow_data + "</p>"
+      + "<p>" + "Data Source: " + feature.properties.data_source + "</p>"
+      + "<p>" + "Real Time Data: <a href=\"http://cdec.water.ca.gov/" + feature.properties.query + "\" target=\"_blank\"> data</a></p>"
+    }
+  
+    if (feature.properties.agency_cd !== undefined) {
+      string +=  
+        "<p>" + "Station Name: " + feature.properties.station_nm + "</p>"
+      + "<p>" + "Station ID: " + feature.properties.site_no + "</p>"
+      + "<p>" + "Site Type: " + feature.properties.site_tp_cd + "</p>"
+      + "<p>" + "Station ID: " + feature.properties.map_nm + "</p>"
+      + "<p>" + "Basin Code: " + feature.properties.basin_cd + "</p>"
+      + "<p>" + "Instruments Code: " + feature.properties.instruments_cd + "</p>"
+      + "<p>" + "RealTime JSON Data: <div url=\"http://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + feature.properties.site_no + "\"  class=\"load-data\">data</div></p>"
+  /*     http://waterservices.usgs.gov/nwis/iv/?format=json&parameterCd=00060,00065&sites=01646500 */
+    }  
+    return string;
 };
 
 water.triggerMapMoveTimeout = function() {
@@ -214,7 +308,7 @@ water.drawStationMarkers = function(features) {
   var featureDetails = {
     name: "station",
     icon: "/images/icons/station_icon.png",
-    layer: "markers_station"
+    layer: "markers_station_cdec"
   };
   
   water.drawMarkers(features, featureDetails);
@@ -231,9 +325,6 @@ water.drawStationUSGSMarkers = function(features) {
   water.drawMarkers(features, featureDetails);
 };
 
-
-
-
 water.makeMarker = function(feature, featureDetails) {
 
   var img = document.createElement('img');
@@ -241,210 +332,4 @@ water.makeMarker = function(feature, featureDetails) {
   img.setAttribute('src', featureDetails.icon);
   img.feature = feature;
   return img;
-
-
-
-/*
-  var id = feature.properties.id;
-  var marker = document.createElement("div");
-  var featureDetails = featureDetails;
-
-  // Unique hash marker id for link
-  marker.setAttribute("id", "marker-" + id);
-  marker.setAttribute("dataName", feature.properties.name);
-  marker.setAttribute("class", "marker " + featureDetails.name);
-
-  // Specially set value for loading data.
-  marker.setAttribute("marker_id", id);
-
-  // create an image icon
-  var img = marker.appendChild(document.createElement("img"));
-
-  if(feature.art) {
-    img.setAttribute("src", feature.art );
-  } else {
-    img.setAttribute("src", featureDetails.icon);
-  }
-*/
-
-
-  
-  
- // return marker;
-//  water.markers.addMarker(marker, feature);
 };
-
-water.makeInteractiveMarker = function(feature, featureDetails) {
-
-
-  var string = '';
-  if (feature.properties.holder_name !== undefined) {
-    string +=  
-      "<p>" + "Owner: " + feature.properties.holder_name + "</p>"
-    + "<p>" + "Type: " + feature.properties.organization_type + "</p>"
-    + "<p>" + "Source: " + feature.properties.source_name + "</p>"
-    + "<p>" + "Watershed: " + feature.properties.watershed + "</p>"
-    + "<p>" + "County: " + feature.properties.county + "</p>"
-    + "<p>" + "Right Type: " + feature.properties.water_right_type + "</p>"
-    + "<p>" + "Right Status: " + feature.properties.status + "</p>"
-    + "<p>" + "Diversion: " + feature.properties.diversion + feature.properties.diversion_units + "</p>"
-    + "<p>" + "Storage: " + feature.properties.diversion_storage_amount + "</p>";
-  }
-
-  if (feature.properties.station_code !== undefined) {
-    string +=  
-      "<p>" + "Station Code: " + feature.properties.station_code + "</p>"
-    + "<p>" + "Station Name: " + feature.properties.station_name + "</p>"
-    + "<p>" + "Station Data Type: " + feature.properties.station_type + "</p>"
-    + "<p>" + "Altitude: " + feature.properties.altitude + "</p>"
-    + "<p>" + "County: " + feature.properties.county + "</p>"
-    + "<p>" + "River Basin: " + feature.properties.river_basin + "</p>"
-    + "<p>" + "Sensors: " + feature.properties.sensors + "</p>"
-    + "<p>" + "Flow Data: " + feature.properties.flow_data + "</p>"
-    + "<p>" + "Data Source: " + feature.properties.data_source + "</p>"
-    + "<p>" + "Real Time Data: <a href=\"http://cdec.water.ca.gov/" + feature.properties.query + "\" target=\"_blank\"> data</a></p>"
-  }
-
-  if (feature.properties.agency_cd !== undefined) {
-    string +=  
-      "<p>" + "Station Name: " + feature.properties.station_nm + "</p>"
-    + "<p>" + "Station ID: " + feature.properties.site_no + "</p>"
-    + "<p>" + "Site Type: " + feature.properties.site_tp_cd + "</p>"
-    + "<p>" + "Station ID: " + feature.properties.map_nm + "</p>"
-    + "<p>" + "Basin Code: " + feature.properties.basin_cd + "</p>"
-    + "<p>" + "Instruments Code: " + feature.properties.instruments_cd + "</p>"
-    + "<p>" + "RealTime JSON Data: <div url=\"http://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + feature.properties.site_no + "\"  class=\"load-data\">data</div></p>"
-/*     http://waterservices.usgs.gov/nwis/iv/?format=json&parameterCd=00060,00065&sites=01646500 */
-  }  
-  
-  // Tooltips
-  $("#marker-" + id + " img").qtip({
-  	content: {
-      text: string,
-  	},
-  	show: {
-  		solo: true,
-  		when: { event: 'unfocus' }
-  	},
-  	hide: {
-  		delay: 5000,
-  		when: { event: 'unfocus' }
-  	},
-  	position: {
-  		my: 'middle left', 
-  		at: 'bottom middle',
-  		adjust: {
-  			x: 20,
-  			y: -10
-  		}
-  	},
-  	style: { 
-  		tip: true,
-  		classes: 'ui-tooltip-dark'
-  	},
-  	tip: {}
-  });
-
-  $('a[title]').qtip();
-  
-  // Listen for mouseover & mouseout events.
-/*
-  MM.addEvent(marker, "mouseover", water.onMarkerOver);
-  MM.addEvent(marker, "mouseout", water.onMarkerOut);
-  MM.addEvent(marker, "click", water.onMarkerClick);
-
-*/
-
-
-};
-
-
-water.getMarker = function(marker) {
-  while (marker && marker.className != "marker") {
-    marker = marker.parentNode;
-  }
-  return marker;
-};
-
-water.onMarkerOver = function(e) {
-  var marker = water.getMarker(e.target);
-  if (marker) {
-    var marker_id = $(this).attr('marker_id');
-    var layer = $(marker).attr("parent");
-    // $('div.marker').css({ 'opacity' : 0.4 }); 
-    // make something pretty now!
-
-
-    // Load data via ajax button
-    $(this).find('.load-data').bind('click', function(){
-  /*     var url = $(this).attr('url'); */
-  /*     console.log(url); */
-      console.log($(this));
-      console.log("test");
-    });
-  }
-};
-
-water.onMarkerOut = function(e) {
-  var marker = water.getMarker(e.target);
-  var layer = $(marker).attr("parent");
-  if (marker) {
-    // var type = marker.type; 
-    // $('div.marker').css({ 'opacity' : 1 }); 
-  }
-  return false;
-};
-
-water.onMarkerClick = function(e) {
-  var marker = e.target.offsetParent;
-  // water.popupMarker(marker);
-  var marker = water.getMarker(e.target);
-  if (marker) {
-    $('div#panel-container div#panel .content').show();
-    console.log(marker);
-    // make something pretty
-  }
-  return false;
-};
-
-
-
-
-
-
-/*
-water.loadPannedMarkers = function() {
-  var center = map.center();
-  var lat = center.lat;
-  var lon = center.lon;
-
-  var boxsize_lat = water.map_defaults.boxsize_lat;
-  var boxsize_lon = water.map_defaults.boxsize_lon;
-    
-  water.markers = 0;
-
-  // Redraw this type of marker in layer if there are features.
-  $('.marker').remove();
-
-
-  // Search for database objects and add markers to map.
-  
-  
-
- Core.query({ 
-     $and: [{'kind': 'right'}, {$where: "this.properties.latitude < " + (lat + boxsize_lat)},{$where: "this.properties.latitude > " + (lat - boxsize_lat)},{$where: "this.properties.longitude < " + (lon + boxsize_lon)},{$where: "this.properties.longitude > " + (lon - boxsize_lon)}
-  ] 
-    }, water.drawRightsMarkers, {'limit': 300});
-
-  Core.query({ 
-   $and: [{'kind': 'station'}, {$where: "this.properties.latitude < " + (lat + boxsize_lat)},{$where: "this.properties.latitude > " + (lat - boxsize_lat)},{$where: "this.properties.longitude < " + (lon + boxsize_lon)},{$where: "this.properties.longitude > " + (lon - boxsize_lon)}
-] 
-  }, water.drawStationMarkers); 
-
-  Core.query({ 
-   $and: [{'kind': 'station_usgs'}, {$where: "this.properties.dec_lat_va < " + (lat + boxsize_lat)},{$where: "this.properties.dec_lat_va > " + (lat - boxsize_lat)},{$where: "this.properties.dec_long_va < " + (lon + boxsize_lon)},{$where: "this.properties.dec_long_va > " + (lon - boxsize_lon)}
-] 
-  }, water.drawStationUSGSMarkers); 
-
-};
-*/
