8a882855b0e1038afff8ce1740aa49e831d65d99
Chacha Sikes <chachasikes@gmail.com>
2013-01-05 20:44:52 -0800

    Fixed mongo error on usgs layer. Some markers may be hidden under cdec markers due to layering

--- public/javascripts/custom/water.map.js
+++ public/javascripts/custom/water.map.js
@@ -9,6 +9,8 @@ water.map_defaults.boxsize_lat = 0.25; //pretty small box
 water.map_defaults.boxsize_lon = 0.5;
 water.map_defaults.zoom = 6;
 water.map_defaults.satellite_layer = 'chachasikes.map-oguxg9bo';
+water.map_defaults.terrain_layer = 'chachasikes.map-tv2igp9l';
+
 water.map_defaults.zoomed_out_marker_layer = 'chachasikes.WaterTransfer-Markers';
 water.map_defaults.div_container = 'map-container';
 water.map_defaults.close_up_zoom_level = 11;
@@ -36,7 +38,7 @@ water.setupMap = function() {
   // Create map.
   water.map = mapbox.map(water.map_defaults.div_container);
   // Add satellite layer.
-  water.map.addLayer(mapbox.layer().id(water.map_defaults.satellite_layer));
+  water.map.addLayer(mapbox.layer().id(water.map_defaults.terrain_layer));
   // Load interactive water rights mapbox layer (has transparent background. Rendered in Tilemill with 45K+ datapoints)        
   mapbox.load(water.map_defaults.zoomed_out_marker_layer, function(interactive){
       water.map.addLayer(interactive.layer);
@@ -45,6 +47,7 @@ water.setupMap = function() {
 
   // Add map interface elements.
   water.map.ui.zoomer.add();
+  
   water.map.setZoomRange(6, 17);  // 17 is the lowest level of satellite layer.
 /*// @TODO see if we can change the increment of the zoomer.
   // This doesn't seem to work though. Maybe make new zoomer? Maybe override easey?
@@ -226,11 +229,11 @@ water.markersQuery = function(reloaded) {
   Core.query({ 
      $and: [{'kind': 'station'}, {$where: "this.properties.latitude < " + (lat + boxsize_lat)},{$where: "this.properties.latitude > " + (lat - boxsize_lat)},{$where: "this.properties.longitude < " + (lon + boxsize_lon)},{$where: "this.properties.longitude > " + (lon - boxsize_lon)}
   ] 
-    }, water.drawStationMarkers); 
+    }, water.drawStationCDECMarkers); 
   
   // Load USGS stations
   Core.query({ 
-     $and: [{'kind': 'station_usgs'}, {'data_type': 'discharge'}, {$where: "this.properties.dec_lat_va < " + (lat + boxsize_lat)},{$where: "this.properties.dec_lat_va > " + (lat - boxsize_lat)},{$where: "this.properties.dec_long_va < " + (lon + boxsize_lon)},{$where: "this.properties.dec_long_va > " + (lon - boxsize_lon)}
+     $and: [{'kind': 'station_usgs'}, {'properties.data_type': 'discharge'},  {$where: "this.properties.dec_lat_va < " + (lat + boxsize_lat)},{$where: "this.properties.dec_lat_va > " + (lat - boxsize_lat)},{$where: "this.properties.dec_long_va < " + (lon + boxsize_lon)},{$where: "this.properties.dec_long_va > " + (lon - boxsize_lon)}
   ] 
     }, water.drawStationUSGSMarkers); 
 };
@@ -319,11 +322,11 @@ water.drawSearchRightsMarkers = function(features) {
   $(".alert .content").html("Found " + features.length + " of 43,000+ water rights.");  
 };
 
-water.drawStationMarkers = function(features) {
-
+water.drawStationCDECMarkers = function(features) {
+  
   var featureDetails = {
     name: "station",
-    icon: "/images/icons/usgs_icon.png",
+    icon: "/images/icons/icon_orange.png",
     layer: "markers_station_cdec"
   };
   
@@ -334,7 +337,7 @@ water.drawStationUSGSMarkers = function(features) {
 
   var featureDetails = {
     name: "station_usgs",
-    icon: "/images/icons/station_icon.png",
+    icon: "/images/icons/icon_brown.png",
     layer: "markers_station_usgs"
   };
   
@@ -351,7 +354,8 @@ water.makeMarker = function(feature, featureDetails) {
 };
 
 water.formatTooltipStrings = function(feature) {
-console.log(feature);
+    //console.log(feature);
+
     var string = '';
     if (feature.properties.holder_name !== undefined) {
       string +=  
@@ -366,6 +370,8 @@ console.log(feature);
       + "<p>" + "Storage: " + feature.properties.diversion_storage_amount + "</p>";
     }
   
+       console.log(feature.properties.station_code);
+
     if (feature.properties.station_code !== undefined) {
       string +=  
         "<p>" + "Station Code: " + feature.properties.station_code + "</p>"
@@ -377,7 +383,8 @@ console.log(feature);
       + "<p>" + "Sensors: " + feature.properties.sensors + "</p>"
       + "<p>" + "Flow Data: " + feature.properties.flow_data + "</p>"
       + "<p>" + "Data Source: " + feature.properties.data_source + "</p>"
-      + "<p>" + "Real Time Data: <a href=\"http://cdec.water.ca.gov/" + feature.properties.query + "\" target=\"_blank\"> data</a></p>"
+      + "<p>" + "Real Time Data: <a href=\"http://cdec.water.ca.gov/" + feature.properties.query + "\" target=\"_blank\"> data</a></p>" +
+      + "<p>" + "URL Preview: http://cdec.water.ca.gov/" + feature.properties.query + "</p>";
     }
 
     if (feature.properties.agency_cd !== undefined) {
@@ -388,7 +395,9 @@ console.log(feature);
       + "<p>" + "Station ID: " + feature.properties.map_nm + "</p>"
       + "<p>" + "Basin Code: " + feature.properties.basin_cd + "</p>"
       + "<p>" + "Instruments Code: " + feature.properties.instruments_cd + "</p>"
-      + "<p>" + "RealTime JSON Data: <div url=\"http://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + feature.properties.site_no + "\"  class=\"load-data\">data</div></p>"
+      + "<p>" + "RealTime JSON Data: <div url=\"http://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + feature.properties.site_no + "\"  class=\"load-data\">data</div></p>" +
+      + "<p>" + "URL Preview: http://waterservices.usgs.gov/nwis/iv/?format=json&sites=" + feature.properties.site_no + "</p>";
+
   /*     http://waterservices.usgs.gov/nwis/iv/?format=json&parameterCd=00060,00065&sites=01646500 */
     }  
     return string;
