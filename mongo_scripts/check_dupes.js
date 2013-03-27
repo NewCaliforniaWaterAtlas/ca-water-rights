bec3113fc55e0c3dcdea9abb9d7a6038e330e404
Chacha Sikes <chachasikes@gmail.com>
2013-03-27 10:31:42 -0700

    remove duplicate records

--- mongo_scripts/check_dupes.js
+++ mongo_scripts/check_dupes.js
@@ -1,8 +1,8 @@
 m = function () {
-    emit(this.properties.application_pod, 1);
+    emit(this.kind, 1);
 }
 
 r = function (k, vals) {
     return Array.sum(vals);
 }
-res = db.database.mapReduce(m, r, { out : "duplicates" });
+res = db.database.mapReduce(m, r, { out : "database_dupes" });
